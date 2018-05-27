b0ec34240269fb6c1d04beba7bbf3a89e88dee47
Sascha <sascha@cbs-service.net>
2018-05-27 16:27:50 +0200

    Change folder of deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SRC=~/git/blog.farb-foto.de/public/
+SRC=~/git/blog/public/
 DST=ssh-w01250de@w00b42f4.kasserver.com:/www/htdocs/w01250de/blog.farb-foto.de/
 
 rsync -azP --exclude '.DS_Store' $SRC $DST
